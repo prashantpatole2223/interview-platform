3865d61e8eb8f6bbc9ba0af7f12dbf5e11bf8447
Prashant Patole <prashantpatole2223@gmail.com>
2026-09-05 19:21:11 +0530

    debug production routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ import {
     useLocation
 } from "react-router-dom";
 
+console.log(" APP LOADED");
+    console.log("Current URL:", window.location.href);
+    console.log("Current pathname:", window.location.pathname);
+    console.log("Current location:", location);
 
 import Navbar from "./components/Navbar/Navbar";
 
