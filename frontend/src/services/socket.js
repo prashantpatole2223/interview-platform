96cabdfd2c9f04921d37d94e4fb8d4adb5e80d9f
Prashant Patole <prashantpatole2223@gmail.com>
2026-09-05 20:58:08 +0530

    fix frontend backend environment variable

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-    import.meta.env.VITE_SOCKET_URL,
+    import.meta.env.BACKEND_SOCKET_URL,
     {
         withCredentials: true,
         autoConnect: false
